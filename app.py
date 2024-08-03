0f8453bb7b925cfab02814ac3063f29ceba9d093
Srujan-rai <raisrujan@gmail.com>
2024-08-03 19:34:18 +0530

    file added

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-from flask import Flask,render_template,redirect,url_for
+from flask import Flask,render_template,redirect,url_for,request,Response,jsonify
+
+app=Flask(__name__)
+
+@app.route('/query-response',methods=['POST','GET'])
+def home():
+    if request.method=='POST':
+        query=request.form['query']
+        print(query)
+    return jsonify({"query entered is ":query})
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True,host='0.0.0.0')
